d43def13f132a284f9e809d1440a2055590c2488
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-05-13 15:17:07 -0600

    Change bg, change fill-rule

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <Layout>
       <Hero />
+      <p className="bg-white text-black dark:text-white dark:bg-black opacity-50 w-fit p-1 my-1 rounded-xl">
+        Please refresh the page to save the changes.
+      </p>
       <Extensions />
     </Layout>
   )
